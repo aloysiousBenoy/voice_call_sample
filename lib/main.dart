e0f566942fe01c7ef42e9a46e4d6c8f354fbc2ec
Aloysious Benoy <aloysiousbenoy@gmail.com>
2020-08-16 00:49:43 +0530

    voice calll home screen complete

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: VoiceHome(),
+    );
+  }
+}
+
+class VoiceHome extends StatefulWidget {
+  @override
+  _VoiceHomeState createState() => _VoiceHomeState();
+}
+
+class _VoiceHomeState extends State<VoiceHome> {
+  TextEditingController callCode = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "DinoMeet",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 32,
+                  fontFamily: 'monospace',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.grey[200],
+                ),
+                width: 200,
+                child: TextField(
+                  style: TextStyle(color: Colors.blueGrey),
+                  controller: callCode,
+                  onChanged: (value) {
+                    print(value);
+                  },
+                ),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  print(callCode.text);
+                },
+                child: Text("Join Call"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
